e309746756a10c52d83bd299e663cb7bbba2952d
esuau <evan.suau@etu.u-pec.fr>
2019-05-15 23:51:00 +0200

    feat(hello-world): Set content-type to JSON

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Example {
 
     public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Content-Type", "application/json");
         PrintWriter writer = response.getWriter();
         writer.write("{\"message\": \"Hello world!\"}");
     }
